6db82442ca2306019924c9cabc6c08f445482b3c
keisuke_karino <keisuke_karino@yayoi-kk.co.jp>
2017-03-15 18:25:44 +0900

    #11:アクティブボタンの切り替え

--- 20170223_20170316/team2/Phase2/html/gulpfile.js
+++ 20170223_20170316/team2/Phase2/html/gulpfile.js
@@ -1,8 +1,18 @@
     var gulp = require('gulp');
     var sass = require('gulp-sass');
+    var autoprefixer = require("gulp-autoprefixer");
 
-    gulp.task('default', function() {
-      gulp.src('scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css')); 
+    gulp.task('default', function () {
+        gulp.src('scss/**/*.scss')
+            .pipe(sass())
+            .pipe(autoprefixer({
+                browsers: ["last 3 versions", "ie >= 9", "Android >= 4", "ios_saf >= 8"],
+                cascade: false
+            }))
+            .pipe(gulp.dest('css'));
+    });
+
+    // 追記する
+    gulp.task('watch', function () {
+        gulp.watch('scss/**/*.scss', ['default']);
     });
