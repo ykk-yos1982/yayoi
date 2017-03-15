f31b4106246297ded84425da5c267d14355897cb
uetk <satomi_uetake@yayoi-kk.co.jp>
2017-03-15 18:26:01 +0900

    マップをクリックで地図を表示する

--- 20170223_20170316/team3/フェーズ2/app/js/apicall.js
+++ 20170223_20170316/team3/フェーズ2/app/js/apicall.js
@@ -1,8 +1,80 @@
 function citySelected(cityParam) {
   // 地図表示
+  console.log('citySelectedが呼ばれました。地図を表示します。');
+  displaymap(cityParam);
   // 天気表示  
 }
 
+// 地図表示
+function displaymap(cityParam){
+
+	var ido   = cityParam.latitude;
+	var keido = cityParam.longitude;
+
+	console.log('ido:' + ido);
+	console.log('keido:' + keido);
+
+	// LatLngLiteralを作成
+	var LatLngLiteral = {
+		lat: ido,
+		lng: keido
+	} ;
+	// 初期化用の関数
+	function initFunc () {
+		// キャンパスの要素を取得する
+		var canvas = document.getElementById( 'map-canvas' ) ;
+
+		// 返り値の案内を空にする
+		returnFunc( "" ) ;
+
+		// 地図のインスタンスを作成する
+		map = new google.maps.Map( canvas, {
+			zoom: 15 ,
+			center: new google.maps.LatLng( ido, keido ) ,
+		} ) ;
+	} ;
+
+	// メソッドボタンのイベント
+	document.getElementById( "method" ).onclick = function () {
+		// 実行
+		var result = map.setCenter( LatLngLiteral ) ;
+
+		// 返り値をコンソールに表示
+		console.log( "返り値:", result ) ;
+
+		// 返り値を表示
+		returnFunc( result ) ;
+	}
+
+	// 返り値表示用の関数
+	function returnFunc ( value ) {
+		switch ( typeof value ) {
+			case "undefined" :
+				value = "undefined" ;
+			break ;
+
+			case "null" :
+				value = "null" ;
+			break ;
+
+			case "object" :
+				try {
+					value = JSON.stringify( value ) ;
+				} catch (e) {
+				}
+			break ;
+		}
+
+		//document.getElementById( "return" ).textContent = value.toString() ;
+	}
+
+	// 地図の表示開始
+	initFunc() ;
+}
+
+
+
+// 天気表示
 $(function() {
 
   // var cityParam = {
