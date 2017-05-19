789d734395b1f0bbe8ee2726df5c8d14857d0c1c
uetk <satomi_uetake@yayoi-kk.co.jp>
2017-05-19 20:32:49 +0900

    既存のトレーニング資料以外を、一時的にFASTUX以下に移動

--- トレーニング教材/フェーズ1/4.セレクタトレーニング/gulpfile.js
+++
@@ -1,8 +0,0 @@
-    var gulp = require('gulp');
-    var sass = require('gulp-sass');
-
-    gulp.task('default', function() {
-      gulp.src('scss/**/*.scss')
-        .pipe(sass())
-        .pipe(gulp.dest('css')); 
-    });
